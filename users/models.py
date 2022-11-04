2d7441591798a675cb19ad3701068507fd7d73d1
Vadim <niras.sp@gmail.com>
2022-11-04 09:24:39 +0700

    add role admin to user, 1.2

--- users/models.py
+++ users/models.py
@@ -16,7 +16,8 @@ class Location(models.Model):
 
 class User(models.Model):
     ROLE = [('member', 'участник'),
-            ('moderator', 'модератор')]
+            ('moderator', 'модератор'),
+            ('admin', 'администратор')]
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50)
